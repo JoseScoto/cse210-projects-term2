2f34cbfc5e3252bb864c585b1a835bf0754947bd
JoseScoto <esc16004@byui.edu>
2025-03-12 23:24:23 -0600

    Resume and jobs activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2000;
+        job1._endYear = 2002;
+        //job1.Display(); Removing these lines that are printing info
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Data Scientist";
+        job2._startYear = 2003;
+        job2._endYear = 2010;
+        //job2.Display(); Removing these lines that are printing info
+
+        Resume myResume = new Resume();
+        myResume._name = "Jose Escoto";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Leaving only the Display method from our Resume class
+        myResume.Display();
     }
 }
